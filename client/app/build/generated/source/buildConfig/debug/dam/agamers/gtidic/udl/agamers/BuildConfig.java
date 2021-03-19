fd4519fef68cdc7b0949490375efced487af4bf5
Ramon Trilla Urteaga <ramontu2001@gmail.com>
2021-03-19 15:27:10 +0100

    Actualització backend
    Moltes coses no estan acabades

---
+++ client/app/build/generated/source/buildConfig/debug/dam/agamers/gtidic/udl/agamers/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dam.agamers.gtidic.udl.agamers;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "dam.agamers.gtidic.udl.agamers";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
