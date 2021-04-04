43e268b0e25e17a071959e387386beecabd89bcc
Ramon Trilla Urteaga <ramontu2001@gmail.com>
2021-04-04 15:05:24 +0200

    Acabat
    Falta documentació i redirecció  automàtica pero ja funciona

--- client/app/src/main/java/dam/agamers/gtidic/udl/agamers/views/MainActivity.java
+++ client/app/src/main/java/dam/agamers/gtidic/udl/agamers/views/MainActivity.java
@@ -2,10 +2,12 @@ package dam.agamers.gtidic.udl.agamers.views;
 
 
 import android.os.Bundle;
+import android.util.Log;
 
 import dam.agamers.gtidic.udl.agamers.CommonActivity;
 import dam.agamers.gtidic.udl.agamers.R;
 import dam.agamers.gtidic.udl.agamers.preferences.PreferencesProvider;
+import dam.agamers.gtidic.udl.agamers.viewmodels.MainActivityViewModel;
 
 public class MainActivity extends CommonActivity {
 
@@ -14,9 +16,20 @@ public class MainActivity extends CommonActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         PreferencesProvider.init(this);
-        goTo(LogInActivity.class);
+
+        MainActivityViewModel mainActivityViewModel = new MainActivityViewModel();
+
+        if (mainActivityViewModel.isCurrentLogIn()){
+            Log.d(TAG, "onCreate() -> El usuari té un tocken, redirigint a pantalla principal");
+            //goTo();
+        }
+        else{
+            goTo(LogInActivity.class);
+        }
+
 
         //TODO fer que es redireccioni a la principal o a login si te token o no
+
     }
 }
 
