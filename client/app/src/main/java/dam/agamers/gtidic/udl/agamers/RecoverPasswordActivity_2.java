865452436dc66323804e8b0c6da3f7a75d8e00c8
Ramon Trilla Urteaga <ramontu2001@gmail.com>
2021-04-16 07:29:25 +0200

    Afegides les pantalles

---
+++ client/app/src/main/java/dam/agamers/gtidic/udl/agamers/RecoverPasswordActivity_2.java
@@ -0,0 +1,18 @@
+package dam.agamers.gtidic.udl.agamers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecoverPasswordActivity_2 extends CommonActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recover_password_2);
+        getSupportActionBar().hide();
+    }
+
+
+
+}
---
+++ client/app/src/main/java/dam/agamers/gtidic/udl/agamers/views/RecoverPasswordActivity_1.java
@@ -0,0 +1,21 @@
+package dam.agamers.gtidic.udl.agamers.views;
+
+import android.os.Bundle;
+
+import dam.agamers.gtidic.udl.agamers.CommonActivity;
+import dam.agamers.gtidic.udl.agamers.R;
+
+public class RecoverPasswordActivity_1 extends CommonActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recover_password_1);
+
+    }
+
+
+    private void recover(){
+        
+    }
+}
