a8fa776ca0bd7d709823bd3778c350145b3b2d91
Ramon Trilla Urteaga <ramontu2001@gmail.com>
2021-04-16 13:55:56 +0200

    prova amb date

--- backend/DAM-ProjectCore/db/models.py
+++ backend/DAM-ProjectCore/db/models.py
@@ -213,7 +213,7 @@ class User(SQLAlchemyBase, JSONModel):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    created_at = Column(datetime.datetime, default=datetime.datetime.now)
+    created_at = Column(datetime.date, default=datetime.date.now)
     username = Column(Unicode(50), nullable=False, unique=True)
     account_type = Column(Enum(AccountTypeEnum),default=AccountTypeEnum.free)
     #following_forums = relationship("Jocs", secondary=Following_Forums, back_populates="seguint") #foros que segueixes TODO no funciona
@@ -230,7 +230,7 @@ class User(SQLAlchemyBase, JSONModel):
     tokens = relationship("UserToken", back_populates="user", cascade="all, delete-orphan")
     name = Column(Unicode(50),default="")
     surname = Column(Unicode(50),default="")
-    birthday = Column(datetime.datetime,nullable=False)
+    birthday = Column(datetime.date,nullable=False)
     genere = Column(Enum(GenereEnum), default=GenereEnum.not_specified)
     #phone = Column(Unicode(50))
     photo = Column(Unicode(255), default="") #TODO mirar si funciona
@@ -245,7 +245,7 @@ class User(SQLAlchemyBase, JSONModel):
     @hybrid_property
     def public_profile(self):
         return {
-            "created_at": self.created_at.strftime(settings.DATETIME_DEFAULT_FORMAT),
+            "created_at": self.created_at.strftime(settings.DATE_DEFAULT_FORMAT),
             "username": self.username,
             "account_type": self.account_type.value,
             "genere": self.genere.value,
