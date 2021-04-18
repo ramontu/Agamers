1a2093de0db371277f932faf41be1cc58be59996
Ramon Trilla Urteaga <ramontu2001@gmail.com>
2021-04-18 16:14:30 +0200

    Bug #64  Fixed

--- backend/DAM-ProjectCore/resources/account_resources.py
+++ backend/DAM-ProjectCore/resources/account_resources.py
@@ -250,7 +250,7 @@ class ResourceAccountRecovery(DAMCoreResource):
 
             <!-- start preheader -->
             <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
-                A preheader is the short summary text that follows the subject line when an email is viewed in the inbox.
+                Here you have your code!
             </div>
             <!-- end preheader -->
 
