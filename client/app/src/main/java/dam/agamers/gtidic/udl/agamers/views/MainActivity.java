704fc812d3f074f7115303bcbab52bd23dd0f7bf
Ramon Trilla Urteaga <ramontu2001@gmail.com>
2021-05-09 17:14:52 +0200

    Ja funciona

--- client/app/src/main/java/dam/agamers/gtidic/udl/agamers/views/MainActivity.java
+++ client/app/src/main/java/dam/agamers/gtidic/udl/agamers/views/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends CommonActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         PreferencesProvider.init(this);
 
         MainActivityViewModel mainActivityViewModel = new MainActivityViewModel();
